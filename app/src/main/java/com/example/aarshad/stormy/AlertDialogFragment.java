220fda0a707c2039d5c260c4c66ab2257be268be
Arslan Arshad <aac.1989@hotmail.com>
2017-04-12 17:29:06 +0900

    End Version 1

--- app/src/main/java/com/example/aarshad/stormy/AlertDialogFragment.java
+++ app/src/main/java/com/example/aarshad/stormy/AlertDialogFragment.java
@@ -3,6 +3,7 @@ package com.example.aarshad.stormy;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
 
 /**
@@ -12,7 +13,13 @@ import android.os.Bundle;
 public class AlertDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
     }
 }
